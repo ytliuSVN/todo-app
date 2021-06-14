c807b926514b43039a44223ec3508ed2ce3fc0c1
penny.liu <penny.liu@kaiostech.com>
2021-06-14 12:01:20 +0800

    :sparkles: Set up fundamental style

--- App.js
+++ App.js
@@ -2,20 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//styled components
+import { Container } from './styles/appStyles';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Text>Swipeable Todo App</Text>
+      <StatusBar style='light' />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/appStyles.js
@@ -0,0 +1,162 @@
+import styled from 'styled-components';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
+import Constants from 'expo-constants';
+
+const statusBarHeight = Constants.statusBarHeight;
+
+// Colors
+export const colors = {
+  primary: '#6f02b5',
+  secondary: '#aa55e0',
+  tertiary: '#E6E6E6',
+  alternative: '#999999',
+};
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${colors.primary};
+  padding: 20px;
+  padding-bottom: 0px;
+  padding-top: ${statusBarHeight}px;
+`;
+
+// Header
+export const HeaderView = styled.View`
+  padding-vertical: 10px;
+  margin-bottom: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: 35px;
+  font-weight: bold;
+  color: ${colors.tertiary};
+  letter-spacing: 2px;
+  font-style: italic;
+`;
+export const HeaderButton = styled.TouchableOpacity`
+  font-weight: bold;
+  color: ${colors.tertiary};
+`;
+
+// List
+export const ListContainer = styled.View`
+  margin-bottom: 30px;
+  flex: 1;
+  padding-bottom: 40px;
+`;
+
+export const ListView = styled.TouchableHighlight`
+  background-color: ${colors.secondary};
+  min-height: 85px;
+  width: 100%;
+  padding: 15px;
+  justify-content: space-around;
+  margin-bottom: 15px;
+  border-radius: 10px;
+`;
+
+export const ListViewHidden = styled.View`
+  background-color: ${colors.tertiary};
+  min-height: 85px;
+  width: 100%;
+  padding: 15px;
+  justify-content: center;
+  align-items: flex-start;
+  margin-bottom: 15px;
+  border-radius: 11px;
+`;
+
+export const HiddenButton = styled.TouchableOpacity`
+  width: 55px;
+  align-items: center;
+`;
+
+export const TodoText = styled.Text`
+  font-size: 16px;
+  letter-spacing: 1px;
+  color: ${colors.tertiary};
+`;
+
+export const TodoDate = styled.Text`
+  font-size: 10px;
+  letter-spacing: 1px;
+  color: ${colors.alternative};
+  text-align: right;
+  text-transform: uppercase;
+`;
+
+// Text for swiped todo row
+export const SwipedTodoText = styled(TodoText)`
+  color: ${colors.alternative};
+  font-style: italic;
+  text-decoration: line-through;
+`;
+
+// Modal
+export const ModalButton = styled.TouchableOpacity`
+  width: 60px;
+  height: 60px;
+  background-color: ${colors.tertiary};
+  border-radius: 50px;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  position: absolute;
+  bottom: 15px;
+`;
+
+export const ModalContainer = styled.View`
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  background-color: ${colors.primary};
+`;
+
+export const ModalView = styled.View`
+  background-color: ${colors.secondary};
+  border-radius: 20px;
+  padding: 35px;
+`;
+
+export const StyledInput = styled.TextInput`
+  width: 300px;
+  height: 50px;
+  background-color: ${colors.tertiary};
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 10px;
+  color: ${colors.secondary};
+  letter-spacing: 1px;
+`;
+
+export const ModalAction = styled.TouchableOpacity`
+  width: 60px;
+  height: 60px;
+  background-color: ${(props) => props.color};
+  border-radius: 50px;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+`;
+
+export const ModalActionGroup = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 30px;
+`;
+
+export const ModalIcon = styled.View`
+  align-items: center;
+  margin-bottom: 30px;
+`;
